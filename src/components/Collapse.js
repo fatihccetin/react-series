2da2406c74a31655479a54e0c79954ebfcf3d669
fatihccetin <48396840+fatihccetin@users.noreply.github.com>
2021-10-01 00:58:23 +0300

    showcontent

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -2,17 +2,29 @@ import React from "react";
 import '../sass/index.scss';
 
 class Collapse extends React.Component {
+    constructor(){
+        super();
+
+        this.state={
+            showContent: true
+        }
+    }
 
     render(){
         return (
             <div>
-                <a className="btn btn-primary w-100"  data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
-                   {/* concat iki string birleştirme */}
+                <a className="btn btn-primary w-100">
+                   {/* 
+                "  data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample"concat iki string birleştirme */}
                     Link with href
                 </a>
-                <div className="collapse show" id={this.props.href}>
-                          {this.props.children}
-                </div>
+                {
+                    this.state.showContent ? (
+                    <div className="collapse show" >
+                    {this.props.children}
+                       </div>) : null
+                }
+                
             </div>
         
         );
