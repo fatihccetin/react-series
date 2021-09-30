ff6553c0e2c0716d115756ae194cd883afbf3f83
fatihccetin <48396840+fatihccetin@users.noreply.github.com>
2021-10-01 00:17:59 +0300

    calss component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,26 @@
 import React from "react";
 import '../sass/index.scss';
 
-const Card = (props) => {
-    console.log(props);
-    return (
-        
-            <div className="card">
-                    <img src={props.image} className="card-img-top" alt="..." />
-                    <div className ="card-body">
-                    <h5 className ="card-title">{props.cardTitle}</h5>
-                    <p className ="card-text">{props.cardText}</p>
-                    </div>
-                    <div className ="card-footer">
-                    <small className ="text-muted">{props.updatedTime}</small>
-                    </div>
+class Card extends React.Component {
+    render() {
+        return (
+
+            <div className="card w-100">
+                <img src={this.props.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.cardTitle}</h5>
+                    <p className="card-text">{this.props.cardText}</p>
                 </div>
-        
-    );
-};
+                <div className="card-footer">
+                    <small className="text-muted">{this.props.updatedTime}</small>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+
 
 export default Card;
+
--- src/index.js
+++ src/index.js
@@ -10,32 +10,44 @@ import Collapse from './components/Collapse';
 const App = () => {
 
     return (
-        <>
-            <div className="card-group">
-                
-                <Collapse href="collapseExample1">
-                    <Card cardTitle="İzmir"
-                        cardText="Lorem Ipsum I"
-                        updatedTime="Last update 1 min "
-                        image="https://picsum.photos/id/1/200/300" />
-                </Collapse>
+        <div className="container">
 
-                <Collapse href="collapseExample2">
-                    <Card cardTitle="Adana"
-                        cardText="Lorem Ipsum II"
-                        updatedTime="Last update 2 min "
-                        image="https://picsum.photos/id/10/200/300" />
-                </Collapse>
+            <div className="row">
+                <div className="card-group w-100">
 
-                <Collapse href="collapseExample3">
-                    <Card cardTitle="urfa"
-                        cardText="Lorem Ipsum III"
-                        updatedTime="Last update 3 min "
-                        image="https://picsum.photos/id/100/200/300" />
-                </Collapse>
+                    <div className="col">
 
+                        <Collapse href="collapseExample1">
+                            <Card cardTitle="İzmir"
+                                cardText="Lorem Ipsum I"
+                                updatedTime="Last update 1 min "
+                                image="https://picsum.photos/id/1/200/300" />
+                        </Collapse>
+                    </div>
+                    <div className="col">
+
+                        <Collapse href="collapseExample2">
+                            <Card cardTitle="Card Title2"
+                                cardText="Lorem Ipsum II"
+                                updatedTime="Last update 2 min "
+                                image="https://picsum.photos/id/10/200/300" />
+                        </Collapse>
+                    </div>
+
+                    <div className="col">
+
+                        <Collapse href="collapseExample3">
+                            <Card cardTitle="card Title 3"
+                                cardText="Lorem Ipsum III"
+                                updatedTime="Last update 3 min "
+                                image="https://picsum.photos/id/100/200/300" />
+                        </Collapse>
+                    </div>
+
+                </div>
             </div>
-        </>
+
+        </div>
     );
 }
 
